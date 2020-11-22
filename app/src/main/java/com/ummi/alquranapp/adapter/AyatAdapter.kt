95fba53d454dfc17e18e15bc70f22bf5bbdec661
ummikolbia22 <miikolbia@gmail.com>
2020-11-22 16:36:37 +0700

    Initial commit

---
+++ app/src/main/java/com/ummi/alquranapp/adapter/AyatAdapter.kt
@@ -0,0 +1,35 @@
+package com.ummi.alquranapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aplikasialquran.model.ModelAyat
+import com.ummi.alquranapp.R
+
+class AyatAdapter(private val items: List<ModelAyat>) : RecyclerView.Adapter<AyatAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_item_ayat, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = items[position]
+        holder.tvNomorAyat.text = data.nomor
+        holder.tvArabic.text = data.arab
+        holder.tvTerjemahan.text = data.indo
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    //Class Holder
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvNomorAyat: TextView = itemView.findViewById(R.id.tvNomorAyat)
+        var tvArabic: TextView = itemView.findViewById(R.id.tvArabic)
+        var tvTerjemahan: TextView = itemView.findViewById(R.id.tvTerjemahan)
+
+    }
+}
